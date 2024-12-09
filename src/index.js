edd5f83985feb5b1a22002a2b0d1a53d43ebb695
cameleon-security <146135356+cameleon-security@users.noreply.github.com>
2024-12-09 23:40:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Desactivate from './components/Desactivate';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Desactivate></Desactivate>
   </React.StrictMode>
 );
