15d0032fb069e1626fd40cd01c4f197c4596a889
cameleon-security <146135356+cameleon-security@users.noreply.github.com>
2024-12-09 23:39:24 +0100

    desactivate feature

---
+++ src/components/Desactivate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Desactivate = () => {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center mx-5">
+      <h1 className="text-5xl font-bold text-gray-800 mb-4">404</h1>
+      <p className="text-gray-700 text-xl mb-4 text-center">
+      Oups ! Le site web de CAMELEON Agence de detective privé est actuellement indisponible. Nous travaillons pour réactiver le site web dès que possible. Merci pour votre patience et votre compréhension. </p>
+    </div>
+  );
+};
+
+export default Desactivate;
